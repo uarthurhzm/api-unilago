9fbc9d0fcd47b1fdb4fa23aca2b7f814054acf76
Arthur <arthurzhm2@gmail.com>
2025-10-16 09:20:04 -0300

    REFACTOR: Ajusta a prioridade e título na consulta de avisos

--- app/src/Domain/Notice/Repository/NoticeRepository.php
+++ app/src/Domain/Notice/Repository/NoticeRepository.php
@@ -10,7 +10,7 @@ class NoticeRepository
     public function GetNotices(GetNoticesDTO $data)
     {
         $filter_id = $data->last_id ? " AND MURAL.ID_MURAL < :LAST_ID " : "";
-        
+
         $execute = [
             ':CD_MAT' => $data->cd_mat,
             ':CD_CSO' => $data->cd_cso
@@ -24,8 +24,8 @@ class NoticeRepository
             "SELECT FIRST 50    
                 MURAL.RECADO,
                 MURAL.DATA,
-                COALESCE(MURAL.PRIORIDADE, 'Baixa') AS PRIORIDADE,
-                'AVISO' AS TITULO,
+                COALESCE(MURAL.PRIORIDADE, '1') AS PRIORIDADE,
+                COALESCE(MURAL.ASSUNTO, 'AVISO') AS TITULO,
                 MURAL_DOCS.URL_DOC,
                 COALESCE(PROFESSOR.NM_PRO, ALUNO.NM_ALU) AS USUARIO
             FROM
@@ -37,7 +37,8 @@ class NoticeRepository
                 LEFT JOIN ALUNO ON
                     ALUNO.CD_MAT = MURAL.FROMTO 
             WHERE 
-                (MURAL.CD_MAT IS NULL OR MURAL.CD_MAT = :CD_MAT)
+                MURAL.RECADO IS NOT NULL
+                AND (MURAL.CD_MAT IS NULL OR MURAL.CD_MAT = :CD_MAT)
                 AND (MURAL.CD_CSO IS NULL OR MURAL.CD_CSO = :CD_CSO)
                 $filter_id
             ORDER BY 
